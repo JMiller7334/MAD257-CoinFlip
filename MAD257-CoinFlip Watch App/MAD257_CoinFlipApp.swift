b3d167430ba284dc206cb6d9e893a4af63893898
jmiller7334 <jmiller7334@students.mchenry.edu>
2023-02-23 14:47:08 -0600

    Initial Commit

---
+++ MAD257-CoinFlip Watch App/MAD257_CoinFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAD257_CoinFlipApp.swift
+//  MAD257-CoinFlip Watch App
+//
+//  Created by student on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MAD257_CoinFlip_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
